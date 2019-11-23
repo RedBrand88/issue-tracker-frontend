a8300044c9d680ebaa118dce6381326c75c31f87
Brandon Bashein <brandon@thefoodtruckleague.com>
2019-11-22 21:05:43 -0700

    added ticket listview

---
+++ src/Components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { AppBar, Toolbar, IconButton, Typography, Button } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import 'typeface-roboto';
+
+function Header(props) {
+    return (
+        <div style={{ flexGrow: 1 }}>
+            <AppBar position="fixed" style={{ background: 'linear-gradient(140deg, #020024 30%, #8e3508 70%, #f9ff00 100%' }}>
+                <Toolbar>
+                    <IconButton edge="start" color="inherit" aria-label="menu">
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h4" style={{ flexGrow: 1 }}>
+                        Code Forge
+                    </Typography>
+                    <Button color="inherit">Login</Button>
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
+}
+
+export default Header;
