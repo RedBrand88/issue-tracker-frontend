61a5f297e9a4f71836d4b45df537d9bbeeee7c4b
Brandon Bashein <brandon@thefoodtruckleague.com>
2019-11-25 00:44:50 -0700

    got rid of breadcrumb event handler

--- src/Components/Breadcrumb.js
+++ src/Components/Breadcrumb.js
@@ -3,15 +3,10 @@ import Typography from '@material-ui/core/Typography';
 import Breadcrumbs from '@material-ui/core/Breadcrumbs';
 import Link from '@material-ui/core/Link';
 
-function handleClick(event) {
-  event.preventDefault();
-  console.info('You clicked a breadcrumb.');
-}
-
 export default function SimpleBreadcrumbs() {
   return (
     <Breadcrumbs aria-label="breadcrumb">
-      <Link color="inherit" href="/" onClick={handleClick}>
+      <Link color="inherit" href="/">
         Home
       </Link>
       <Typography color="textPrimary">Tickets</Typography>
