f6f004a83bc4214c1616ec29e0d66f145d91b42e
RedBrand88 <brandon@thefoodtruckleague.com>
2020-04-16 00:38:39 -0600

    initial refactor of layout container

--- src/Containers/Layout.js
+++ src/Containers/Layout.js
@@ -1,27 +1,24 @@
 import React from 'react';
-import Grid from '@material-ui/core/Grid';
 import { IconButton } from '@material-ui/core';
 import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
-import Breadcrumb from '../Components/Breadcrumb';
 
 class Layout extends React.Component {
 
     render() {
         const BASE_URL = 'staging.theprojectforge.com';
         return (
-            <Grid container spacing={3} style={{ background: '#020024', color: 'white' }}>
-                <Grid item xs={12}>
+            <div style={{ background: '#020024', color: 'white' }}>
+                <div>
                     <div style={{ width: '75%', marginRight: 'auto', marginLeft: 'auto', marginTop: 100 }}>
-                        <Breadcrumb />
                         <IconButton href={'http://'+BASE_URL+'/create-ticket'} style={{ float: 'right', color: 'white' }}>
                             <AddCircleOutlineIcon fontSize='large' />
                         </IconButton>
                     </div>
-                </Grid>
-                <Grid item xs={12}>
+                </div>
+                <div>
                     {this.props.children}
-                </Grid>
-            </Grid>
+                </div>
+            </div>
         );
     }
 }
