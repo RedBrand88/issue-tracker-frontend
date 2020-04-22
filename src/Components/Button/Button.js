e1c4b24b657838a454ed6f13f86b7fb415a936c6
RedBrand88 <brandon@thefoodtruckleague.com>
2020-04-21 18:24:29 -0600

    add button component

---
+++ src/Components/Button/Button.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './Button.module.css';
+
+const Button = (props) => {
+    return (
+    <button type='button' className={styles.btn}>{props.text}</button>
+    )
+}
+
+export default Button;
